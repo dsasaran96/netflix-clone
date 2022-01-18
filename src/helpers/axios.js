f12f95a5c5f33f751d303944961294084fbda5b2
Dragos Sasaran <dragos.sasaran@gmail.com>
2022-01-19 01:26:15 +0200

    fixed axios

--- src/helpers/axios.js
+++ src/helpers/axios.js
@@ -1,14 +1,7 @@
 import axios from 'axios'
 
 const instance = axios.create({
-<<<<<<< HEAD
-    baseURL: "https://api.themoviedb.org/3"
-})
-
-export default instance;
-=======
     baseURL: "http://api.themoviedb.org/3",
 })
 
 export default instance
->>>>>>> features
