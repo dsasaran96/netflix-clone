6d962d4709dc7db63956982097d50d2093af47c2
Dragos Sasaran <dragos.sasaran@gmail.com>
2022-01-19 01:25:12 +0200

    merge

--- src/utils/requests.js
+++ src/utils/requests.js
@@ -1,19 +1,5 @@
 const API_KEY = '7cfc1357043eaf66ae6f3d94546704e4'
 
-<<<<<<< HEAD
-const requests = {
-    fetchTrending: `/trending/all/week?api_key=${API_KEY}&language=en-us`,
-    fetchNetflixOriginals: `/discover/tv?api_key=${API_KEY}&with_networks=213`,
-    fetchTopRated: `/movie_top_rated?api_key=${API_KEY}&language=en-US`,
-    fetchActionMovies: `/discover/movie?api_key=${API_KEY}&with_genres=28`,
-    fetchComedyMovies: `/discover/movie?api_key=${API_KEY}&with_genres=35`,
-    fetchHorrorMovies: `/discover/movie?api_key=${API_KEY}&with_genres=27`,
-    fetchRomanceMovies: `/discover/movie?api_key=${API_KEY}&with_genres=10749`,
-    fetchDocumentaries: `/discover/movie?api_key=${API_KEY}&with_genres=99`
-}
-
-export default requests
-=======
 //drama, thriller, children, suspense, romance - films
 
 const requests = {
@@ -30,4 +16,3 @@ const requests = {
 }
 
 export default requests
->>>>>>> features
