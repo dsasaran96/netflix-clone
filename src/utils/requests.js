646b94238c3633fb012d95cc2761ab1694b93e2d
Dragos Sasaran <dragos.sasaran@gmail.com>
2022-01-22 18:24:19 +0200

    fixed requests

--- src/utils/requests.js
+++ src/utils/requests.js
@@ -1,16 +1,16 @@
 const requests = {
-    fetchDocumentariesSeries: `/discover/tv?api_key=${API_KEY}&with_genres=99`,
-    fetchComedySeries: `/discover/tv?api_key=${API_KEY}&with_genres=35`,
-    fetchChildrenSeries: `/discover/tv?api_key=${API_KEY}&with_genres=10762`,
-    fetchCrimeSeries: `/discover/tv?api_key=${API_KEY}&with_genres=80`,
-    fetchFeelGoodSeries: `/discover/tv?api_key=${API_KEY}&with_genres=10766`,
-    fetchDramaMovies: `/discover/movie?api_key=${API_KEY}&with_genres=18`,
-    fetchThrillerMovies: `/discover/movie?api_key=${API_KEY}&with_genres=53`,
-    fetchChildrenMovies: `/discover/movie?api_key=${API_KEY}&with_genres=16`,
-    fetchSuspenseMovies: `/discover/movie?api_key=${API_KEY}&with_genres=12`,
-    fetchRomanceMovies: `/discover/movie?api_key=${API_KEY}&with_genres=10749`,
+    fetchDocumentariesSeries: `/discover/tv?api_key=${process.env.REACT_APP_API_KEY}&with_genres=99`,
+    fetchComedySeries: `/discover/tv?api_key=${process.env.REACT_APP_API_KEY}&with_genres=35`,
+    fetchChildrenSeries: `/discover/tv?api_key=${process.env.REACT_APP_API_KEY}&with_genres=10762`,
+    fetchCrimeSeries: `/discover/tv?api_key=${process.env.REACT_APP_API_KEY}&with_genres=80`,
+    fetchFeelGoodSeries: `/discover/tv?api_key=${process.env.REACT_APP_API_KEY}&with_genres=10766`,
+    fetchDramaMovies: `/discover/movie?api_key=${process.env.REACT_APP_API_KEY}&with_genres=18`,
+    fetchThrillerMovies: `/discover/movie?api_key=${process.env.REACT_APP_API_KEY}&with_genres=53`,
+    fetchChildrenMovies: `/discover/movie?api_key=${process.env.REACT_APP_API_KEY}&with_genres=16`,
+    fetchSuspenseMovies: `/discover/movie?api_key=${process.env.REACT_APP_API_KEY}&with_genres=12`,
+    fetchRomanceMovies: `/discover/movie?api_key=${process.env.REACT_APP_API_KEY}&with_genres=10749`,
 
-    fetchFeature: `/trending/all/day?api_key=${API_KEY}`
+    fetchFeature: `/trending/all/day?api_key=${process.env.REACT_APP_API_KEY}`
 }
 
 export default requests
